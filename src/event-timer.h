461bac0b6642cb4c165ae1952ee31c97abdc5e66
Peter Trško <peter.trsko@gmail.com>
2014-11-05 21:47:06 +0100

    Commenting Event_timer fields
    These are the most important parts from perspective c-event-machine
    developer, but not from the user point of view.

--- src/event-timer.h
+++ src/event-timer.h
@@ -7,19 +7,33 @@
 extern "C" {
 #endif
 
-struct Event_timer_s;
+struct Event_timer_s;   /* Forward declaration */
 
 typedef void (*Event_timer_handler)(struct Event_timer_s *timer, void *data);
 
 typedef struct Event_timer_s
 {
+    /** Event descriptor used to register timer instance in provided
+     * event machine instance.
+     */
     EM_event_descriptor event_descriptor;
+
+    /** Event machine in which this timer instance was/will be
+     * registered.
+     */
     EM *event_machine;
+
+    /** Private data associated with this instance of timer.
+     *
+     * It's value is passed down to <tt>callback</tt> and it may be
+     * <tt>NULL</tt>.
+     */
     void *data;
+
+    /** Callback which is invoked when timer fires.
+     */
     Event_timer_handler callback;
-
-}Event_timer;
-
+} Event_timer;
 
 uint8_t event_timer_creat(EM *event_machine, Event_timer* timer,
     Event_timer_handler callback, void *data);
